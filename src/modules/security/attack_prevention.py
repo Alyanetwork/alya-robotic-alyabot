bb227d69585646eea1a51719138a24d6b53f0c12
FYS TEKNOLOJİ <83513785+Alyanetwork@users.noreply.github.com>
2024-11-14 04:01:06 +0300

    Create attack_prevention.py

---
+++ src/modules/security/attack_prevention.py
@@ -0,0 +1,6 @@
+# src/modules/security/attack_prevention.py
+
+class AttackPrevention:
+    def activate_defense(self):
+        print("Savunma sistemi aktif. Saldırı önleniyor.")
+        # Güvenlik protokolleri, örneğin alan tarama veya engeller yerleştirme
